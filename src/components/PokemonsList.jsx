d7de916f5baade5d593d9fb509c9b7e90d4e80ca
jleandro92 <jleandro0802@gmail.com>
2024-02-17 01:43:29 -0300

    Renderizando Pokemons

--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -1,9 +1,10 @@
 import PokemonPreview from "./PokemonPreview"
 
 const PokemonsList = ({pokemons}) => {
-    
+
   return (
-    <section>
+    <section className="pt-4 grid grid-cols-[repeat(auto-fit, _minmax(180px, _1fr))]
+    gap-4">
         {
             pokemons.map(pokemon => (
                 <PokemonPreview key={pokemon.url} 
